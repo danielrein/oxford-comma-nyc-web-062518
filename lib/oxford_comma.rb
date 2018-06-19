02b6059b40c6cfed9b4094f5f6c855f91f920c2e
Daniel Ran <danielr1735@gmail.com>
2018-06-19 14:20:21 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,10 @@
 def oxford_comma(array)
-  array[-1] = "and #{array[-1]}"
-  array.join(", ")
+  if array.length <=1
+    string = array.to_s
+  elsif array.length == 2
+    string = "#{array[0]} and #{array[1]}"
+  else
+    array[-1] = "and #{array[-1]}"
+    array.join(", ")
+  end
 end
